050f2bed4374576dd386f160b973b5108d568342
imran-khan-dev <emrankhanniloy@gmail.com>
2025-05-25 16:53:45 +0600

    table data inserted

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,4 +1,4 @@
--- Table 1
+-- Table 1 created
 CREATE TABLE rangers (
     ranger_id SERIAL PRIMARY KEY,
     name VARCHAR(30) NOT NULL,
@@ -11,7 +11,7 @@ SET NOT NULL,
 ALTER COLUMN region
 SET NOT NULL;
 
--- Table 2
+-- Table 2 created
 CREATE TABLE species (
     species_id SERIAL PRIMARY KEY,
     common_name VARCHAR(30) NOT NULL,
@@ -26,7 +26,7 @@ SET NOT NULL,
 ALTER COLUMN conservation_status
 SET NOT NULL;
 
--- Table 3
+-- Table 3 created
 CREATE TABLE sightings (
     sighting_id SERIAL PRIMARY KEY,
     ranger_id INT NOT NULL,
@@ -38,4 +38,92 @@ CREATE TABLE sightings (
     CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCES species (species_id)
 )
 
+-- Table 1 data insert
+INSERT INTO
+    rangers (name, region)
+VALUES (
+        'conservation_status',
+        'Northern Hills'
+    ),
+    ('Bob White', 'River Delta'),
+    (
+        'Carol King',
+        'Mountain Range'
+    )
 
+UPDATE rangers
+SET
+    name = 'Alice Green'
+WHERE
+    name = 'conservation_status';
+
+-- Table 2 data insert
+INSERT INTO
+    species (
+        common_name,
+        scientific_name,
+        discovery_date,
+        conservation_status
+    )
+VALUES (
+        'Snow Leopard',
+        'Panthera uncia',
+        '1775-01-01',
+        'Endangered'
+    ),
+    (
+        'Bengal Tiger',
+        'Panthera tigris tigris',
+        '1758-01-01',
+        'Endangered'
+    ),
+    (
+        'Red Panda',
+        'Ailurus fulgens',
+        '1825-01-01',
+        'Vulnerable'
+    ),
+    (
+        'Asiatic Elephant',
+        'Elephas maximus indicus',
+        '1758-01-01',
+        'Endangered'
+    );
+
+-- Table 3 data insert
+INSERT INTO
+    sightings (
+        species_id,
+        ranger_id,
+        location,
+        sighting_time,
+        notes
+    )
+VALUES (
+        1,
+        1,
+        'Peak Ridge',
+        '2024-05-10 07:45:00',
+        'Camera trap image captured'
+    ),
+    (
+        2,
+        2,
+        'Bankwood Area',
+        '2024-05-12 16:20:00',
+        'Juvenile seen'
+    ),
+    (
+        3,
+        3,
+        'Bamboo Grove East',
+        '2024-05-15 09:10:00',
+        'Feeding observed'
+    ),
+    (
+        1,
+        2,
+        'Snowfall Pass',
+        '2024-05-18 18:30:00',
+        NULL
+    );
